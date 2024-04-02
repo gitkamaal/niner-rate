3850c111f3d0aa6ad81d0b7dacfbc5437956a6eb
Jai_Vang <jaivang0387@gmail.com>
2024-04-02 18:49:45 -0400

    updated pagination component to limit display of page numbers

--- src/components/pagination.tsx
+++ src/components/pagination.tsx
@@ -2,9 +2,13 @@ import React from 'react';
 
 const Pagination = ({ totalPages, currentPage, onPageChange }) => {
   const pageNumbers = Array.from(
-    { length: totalPages },
+    { length: totalPages }, 
     (_, index) => index + 1
   );
+
+  // Calculate the range of pages to display
+  const startPage = Math.max(1, currentPage - 2);
+  const endPage = Math.min(currentPage + 2, totalPages);
 
   return (
     <div className="flex justify-center items-center mt-20 space-x-2">
@@ -16,14 +20,13 @@ const Pagination = ({ totalPages, currentPage, onPageChange }) => {
         Prev
       </button>
 
-      {pageNumbers.map((number) => (
+      {/* Display page numbers based on the range */}
+      {pageNumbers.slice(startPage - 1, endPage).map((number) => (
         <button
           key={number}
           onClick={() => onPageChange(number)}
           className={`px-4 py-2 rounded ${
-            currentPage === number
-              ? 'bg-gray-400'
-              : 'bg-gray-300 hover:bg-gray-400'
+            currentPage === number ? 'bg-gray-400' : 'bg-gray-300 hover:bg-gray-400'
           }`}
         >
           {number}
